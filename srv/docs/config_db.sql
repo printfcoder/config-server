c19edcdc43b6cdc8b0e1e92cd55052423f8933ab
shu xian <i@shux.me>
2019-11-04 22:21:14 +0800

    add db

---
+++ srv/docs/config_db.sql
@@ -0,0 +1,26 @@
+create table app
+(
+    id           int unsigned auto_increment
+        primary key,
+    app_id       varchar(200)                              not null,
+    app_name     varchar(200)                              not null,
+    deleted      tinyint(1)   default 0                    not null,
+    created_time timestamp(3) default CURRENT_TIMESTAMP(3) not null,
+    updated_time timestamp(3) default CURRENT_TIMESTAMP(3) not null on update CURRENT_TIMESTAMP(3),
+    constraint app_app_id_uindex
+        unique (app_id)
+);
+
+create table app_env
+(
+    id           int unsigned auto_increment
+        primary key,
+    name         varchar(200)                              not null,
+    app_id       varchar(200)                              not null,
+    deleted      tinyint(1)   default 0                    not null,
+    created_time timestamp(3) default CURRENT_TIMESTAMP(3) not null,
+    updated_time timestamp(3) default CURRENT_TIMESTAMP(3) not null on update CURRENT_TIMESTAMP(3),
+    constraint app_env_name_app_id_uindex
+        unique (name, app_id)
+);
+
