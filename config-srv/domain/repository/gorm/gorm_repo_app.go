c7465fda68898f32c2bf765e4028fd26143f7e58
Allenxuxu <120582243@qq.com>
2019-11-21 21:13:45 +0800

    add repo app interface

--- config-srv/domain/repository/gorm/gorm_repo_app.go
+++ config-srv/domain/repository/gorm/gorm_repo_app.go
@@ -2,8 +2,7 @@ package gorm
 
 import "github.com/micro-in-cn/config-server/config-srv/domain/model"
 
-type appRepo struct {
-}
+type appRepo struct{}
 
 func (a appRepo) CreateApp(appId, appName string) (int64, error) {
 	db.Select("SELECT * FROM")
@@ -11,5 +10,45 @@ func (a appRepo) CreateApp(appId, appName string) (int64, error) {
 }
 
 func (a appRepo) ListApps(appIds ...string) ([]*model.App, error) {
+	panic("implement me")
+}
+
+func (a appRepo) CreateEnv(appId, envName string) error {
+	panic("implement me")
+}
+
+func (a appRepo) CreateCluster(appId, envName, clusterName string) error {
+	panic("implement me")
+}
+
+func (a appRepo) CreateNamespace(appId, envName, clusterName, namespace string) error {
+	panic("implement me")
+}
+
+func (a appRepo) CreateItems(appId, envName, clusterName, namespace string, items map[string]string) error {
+	panic("implement me")
+}
+
+func (a appRepo) DeleteApp(appId, appName string) (int64, error) {
+	panic("implement me")
+}
+
+func (a appRepo) DeleteEnv(appId, envName string) error {
+	panic("implement me")
+}
+
+func (a appRepo) DeleteCluster(appId, envName, clusterName string) error {
+	panic("implement me")
+}
+
+func (a appRepo) DeleteNamespace(appId, envName, clusterName, namespace string) error {
+	panic("implement me")
+}
+
+func (a appRepo) DeleteItems(appId, envName, clusterName, namespace string, keys ...string) error {
+	panic("implement me")
+}
+
+func (a appRepo) UpdateItems(appId, envName, clusterName, namespace string, items map[string]string) error {
 	panic("implement me")
 }
--- config-srv/domain/repository/repository_app.go
+++ config-srv/domain/repository/repository_app.go
@@ -8,5 +8,18 @@ type Repository interface {
 
 type AppRepository interface {
 	CreateApp(appId, appName string) (int64, error)
+	CreateEnv(appId, envName string) error
+	CreateCluster(appId, envName, clusterName string) error
+	CreateNamespace(appId, envName, clusterName, namespace string) error
+	CreateItems(appId, envName, clusterName, namespace string, items map[string]string) error
+
+	DeleteApp(appId, appName string) (int64, error)
+	DeleteEnv(appId, envName string) error
+	DeleteCluster(appId, envName, clusterName string) error
+	DeleteNamespace(appId, envName, clusterName, namespace string) error
+	DeleteItems(appId, envName, clusterName, namespace string, keys ...string) error
+
+	UpdateItems(appId, envName, clusterName, namespace string, items map[string]string) error
+
 	ListApps(appIds ...string) ([]*model.App, error)
 }
