606b2bb11d90fc37ae7772e030e9833668eed5e9
Allenxuxu <120582243@qq.com>
2019-11-26 15:56:37 +0800

    调整表结构

--- config-srv/domain/model/cluster.go
+++ config-srv/domain/model/cluster.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Cluster struct {
-	ID        uint       `gorm:"column:id;primary_key" json:"id"`
-	CreatedAt time.Time  `gorm:"column:created_at" json:"createdAt"`
-	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updatedAt"`
-	DeletedAt *time.Time `sql:"index" gorm:"column:deleted_at" json:"deletedAt"`
+	ID        uint       `gorm:"column:id;          primary_key" json:"id"`
+	CreatedAt time.Time  `gorm:"column:created_at"               json:"createdAt"`
+	UpdatedAt time.Time  `gorm:"column:updated_at"               json:"updatedAt"`
+	DeletedAt *time.Time `sql:"index" gorm:"column:deleted_at"   json:"deletedAt"`
 
-	Name  string `gorm:"column:name;not null" json:"name"`
-	AppID string `gorm:"column:app_id;index:cluster_app_id_index;not null" json:"appId"`
+	AppName string `gorm:"column:app_name; not null; unique_index:cluster_uindex" json:"appName"`
+	Name    string `gorm:"column:name;     not null; unique_index:cluster_uindex" json:"name"`
 }
